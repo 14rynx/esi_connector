80ae7c7f9c52e2de63e62a35700279719c0b80a0
larynx <larynx.austrene@gmail.com>
2023-10-14 12:23:18 +0200

    Added new features

--- esi.py
+++ esi.py
@@ -133,69 +133,113 @@ class ESIConnector:
         return await self.get_value("universe/systems/", system_id, key="security_status")
 
     @alru_cache(maxsize=2000)
-    async def get_character_corporation(self, character_id) -> int:
-        return await self.get_id("characters/", character_id, key="corporation_id")
+    async def get_system_name(self, system_id) -> str:
+        return await self.get_str(f"universe/systems/", system_id, key="name")
 
     @alru_cache(maxsize=2000)
-    async def get_corp_member_count(self, corporation_id) -> int:
-        return int(await self.get_value("corporations/", corporation_id, key="member_count"))
+    async def get_system_constellation_id(self, system_id) -> int:
+        return await self.get_id(f"universe/systems/", system_id, key="constellation_id")
 
     @alru_cache(maxsize=2000)
-    async def get_jumps(self, origin_system_id, destination_system_id):
-        response = await self.get(f"route/{origin_system_id}/{destination_system_id}/")
+    async def get_system_region_id(self, system_id) -> int:
+        constellation_id = await self.get_system_constellation_id(system_id)
+        return await self.get_id(f"universe/constellations/", constellation_id, key="region_id")
+
+    @alru_cache(maxsize=2000)
+    async def get_system_region_name(self, system_id) -> str:
+        region_id = await self.get_system_region_id(system_id)
+        return await self.get_str(f"universe/regions/", region_id, key="name")
+
+    @alru_cache(maxsize=2000)
+    async def get_system_space_name(self, system_id) -> str:
+        region_id = await self.get_system_region_id(system_id)
+        sec_status = await self.get_system_security(system_id)
+        if system_id > 31000000:
+            return "Wormholes"
+        elif region_id == 10000070:
+            return "Pochven"
+        elif region_id == 10001000:
+            return "Zarzakh"
+        elif sec_status >= 0.5:
+            return "Highsec"
+        elif sec_status > 0:
+            return "Lowsec"
+        else:
+            return "Nullsec"
+
+
+@alru_cache(maxsize=2000)
+async def get_character_corporation(self, character_id) -> int:
+    return await self.get_id("characters/", character_id, key="corporation_id")
+
+
+@alru_cache(maxsize=2000)
+async def get_corp_member_count(self, corporation_id) -> int:
+    return int(await self.get_value("corporations/", corporation_id, key="member_count"))
+
+
+@alru_cache(maxsize=2000)
+async def get_jumps(self, origin_system_id, destination_system_id):
+    response = await self.get(f"route/{origin_system_id}/{destination_system_id}/")
+    try:
+        return len(response)
+    except ValueError:
+        return sys.maxsize
+
+
+@alru_cache(maxsize=2000)
+async def get_gate_destination(self, gate_id):
+    response = await self.get(f"universe/stargates/{gate_id}/")
+    return response["destination"]["system_id"]
+
+
+@alru_cache(maxsize=2000)
+async def get_connected_systems(self, system_id):
+    response = await self.get(f"universe/systems/{system_id}/")
+    try:
+        gates = response["stargates"]
+        destination_tasks = [self.get_gate_destination(gate_id) for gate_id in gates]
+        return await asyncio.gather(*destination_tasks)
+    except KeyError:
+        pass
+    return []
+
+
+async def get_proximity_systems(self, start, max_distance=10):
+    known = [start]
+    front = [start]
+    out = [(start, 0)]
+    for x in range(1, max_distance + 1):
+        connection_tasks = [self.get_connected_systems(s) for s in front]
+        connected = flatten(await asyncio.gather(*connection_tasks))
+        front = list(set([i for i in connected if i not in known]))
+        known.extend(connected)
+        out.extend([(n, x) for n in front])
+    return out
+
+
+# Functions to get a kill from esi
+async def get_kill(self, kill_id, kill_hash, progress_bar=None):
+    async with self.session.get(
+            f"https://esi.evetech.net/latest/killmails/{kill_id}/{kill_hash}/?datasource=tranquility") as response:
         try:
-            return len(response)
-        except ValueError:
-            return sys.maxsize
+            kill = await response.json(content_type=None)
+            if progress_bar:
+                progress_bar.update(1)
+            return kill
+        except json.decoder.JSONDecodeError:
+            return await self.get_kill(kill_id, kill_hash, progress_bar)
 
-    @alru_cache(maxsize=2000)
-    async def get_gate_destination(self, gate_id):
-        response = await self.get(f"universe/stargates/{gate_id}/")
-        return response["destination"]["system_id"]
 
-    @alru_cache(maxsize=2000)
-    async def get_connected_systems(self, system_id):
-        response = await self.get(f"universe/systems/{system_id}/")
-        try:
-            gates = response["stargates"]
-            destination_tasks = [self.get_gate_destination(gate_id) for gate_id in gates]
-            return await asyncio.gather(*destination_tasks)
-        except KeyError:
-            pass
-        return []
+async def gather_kills(self, kills, progress_bar=None):
+    tasks = [self.get_kill(*kill, progress_bar) for kill in kills.items()]
+    return await asyncio.gather(*tasks)
 
-    async def get_proximity_systems(self, start, max_distance=10):
-        known = [start]
-        front = [start]
-        out = [(start, 0)]
-        for x in range(1, max_distance + 1):
-            connection_tasks = [self.get_connected_systems(s) for s in front]
-            connected = flatten(await asyncio.gather(*connection_tasks))
-            front = list(set([i for i in connected if i not in known]))
-            known.extend(connected)
-            out.extend([(n, x) for n in front])
-        return out
 
-    # Functions to get a kill from esi
-    async def get_kill(self, kill_id, kill_hash, progress_bar=None):
-        async with self.session.get(
-                f"https://esi.evetech.net/latest/killmails/{kill_id}/{kill_hash}/?datasource=tranquility") as response:
-            try:
-                kill = await response.json(content_type=None)
-                if progress_bar:
-                    progress_bar.update(1)
-                return kill
-            except json.decoder.JSONDecodeError:
-                return await self.get_kill(kill_id, kill_hash, progress_bar)
-
-    async def gather_kills(self, kills, progress_bar=None):
-        tasks = [self.get_kill(*kill, progress_bar) for kill in kills.items()]
-        return await asyncio.gather(*tasks)
-
-    async def gather_kills_chunked(self, hashes, pbar=None):
-        for chunk in chunks(hashes, 1000):
-            for kill in (await self.gather_kills(chunk, pbar)):
-                yield kill
+async def gather_kills_chunked(self, hashes, pbar=None):
+    for chunk in chunks(hashes, 1000):
+        for kill in (await self.gather_kills(chunk, pbar)):
+            yield kill
 
 
 nest_asyncio.apply()
